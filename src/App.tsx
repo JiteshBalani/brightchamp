f10f1b4db325892b6b0ed58f99e15db89832f2e3
JiteshBalani <balani.jitesh911@gmail.com>
2024-03-06 21:03:03 +0530

    project initialed

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Screen1 from './pages/Screen1'
+
+function App() {
+
+  return  (
+    <div className='w-screen h-screen'>
+      <Screen1/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/Screen1.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import baseImg from '../assets/baseImg.svg'
+
+const Screen1 = () => {
+  return (
+    <div className='w-screen h-screen'>
+        <img src={baseImg} alt='Welcome Screen' />
+    </div>
+  )
+}
+
+export default Screen1
